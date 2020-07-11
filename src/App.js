c4aed9c29d95c7609ad37170445945829998493f
krishan kumar <krishan kumar>
2020-07-11 10:06:35 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -3,18 +3,53 @@ import Person from './Person/Person'
 import './App.css';
 
 class App extends Component {
+
+    state={
+        persons:[
+            {name:"krishan",skill:"FullStack Web Developer"},
+            {name:"Pawan",skill:"FullStack Web Developer"},
+            {name:"Ajay Kadoula",skill:"FullStack Web Developer"},
+            {name:"Rishu Gupta",skill:"FullStack Web Developer"},
+            {name:"Akash",skill:"UI/UX Designer"},
+            {name:"Pankaj",skill:"UI/UX Designer"},
+            {name:"vikas",skill:"React Developer"},
+            {name:"Pankaj",skill:"Andriod Developer"}
+        ],
+
+        otherState:'some other state'
+    }
+
+    switchStateHandler = ()=>{
+  // Dont do this this.state.persons[0].name=maximilian
+         this.setState({
+             persons:[
+                {name:"Rishu Gupta",skill:"FullStack/GITHUB"},
+                {name:"krishan",skill:"FullStack Web Developer"},
+                {name:"Pawan",skill:"FullStack Web Developer"},
+                {name:"Ajay Kadoula",skill:"FullStack Web Developer"},
+                {name:"Akash",skill:"UI/UX Designer"},
+                {name:"Pankaj",skill:"UI/UX Designer"},
+                {name:"vikas",skill:"React Developer"},
+                {name:"Pankaj",skill:"Andriod Developer"}
+               
+             ]
+         })
+   
+    }
+
     render() {
         return ( 
         <div className = "App" >
              
-            < Person name="Krishan" skill="FullStack Web Developer" />
-            < Person name="Pawan" skill="UI/UX Designer" />
-            < Person name="Ajay Kadoula" skill="FullStack Web Developer" />
-            < Person name="Rishu Gupta" skill="Full Stack" />
-            < Person name="Akash" skill="Full Stack" />
-            < Person name="Pankaj" skill="React Developer" />
-            < Person name="vikas" skill="UI/UX Designer " />
-            < Person name="Pawan" skill="UI/UX Designer" />
+                <button onClick={this.switchStateHandler}>Switch Name</button><br/>
+            < Person name={this.state.persons[0].name} skill={this.state.persons[0].skill} />
+            < Person name={this.state.persons[1].name} skill={this.state.persons[1].skill} />
+            < Person name={this.state.persons[2].name} skill={this.state.persons[2].skill} />
+            < Person name={this.state.persons[3].name} skill={this.state.persons[3].skill} />
+            < Person name={this.state.persons[4].name} skill={this.state.persons[4].skill} />
+            < Person name={this.state.persons[5].name} skill={this.state.persons[5].skill} />
+            < Person name={this.state.persons[6].name} skill={this.state.persons[6].skill} />
+            < Person name={this.state.persons[7].name} skill={this.state.persons[7].skill} />
             </div>
         );
     }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,9 +3,9 @@ import './Person.css';
 const Person = (props) => {
     return ( 
         <div className="person">
-           <img src={require("./Images/man.png")} alt="imageErr" className="img"></img>
+           <img src={require("./Images/man.jpg")} alt="imageErr" className="img"></img>
            <h2>{props.name}</h2>
-           <p>{props.skill}</p>
+           <p>{props.skill}</p> 
         </div>
     
     )
